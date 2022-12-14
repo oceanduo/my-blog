c49d0f38fa3db94bd451040794da1f0547734c2f
oceanduo <oceanduo@gmail.com>
2022-12-14 15:37:32 +1000

    basic structure done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Topbar from "./components/topbar/topbar";
 import "./app.scss"
 
 import Single from "./pages/single/single";
+import Write from "./pages/write/Write";
+import Register from "./pages/register/register";
+import Settings from "./pages/setting/Setting";
+import Login from "./pages/login/login";
 
 function App() {
   return (
     <div className="App">
     <Topbar/>
     <Single/>
+    <Write/>
+    <Register />
+    <Settings/>
+    <Login/>
     </div>
   );
 }
